467e1377629ba73e6bb1d4bda730703abd0266dd
ByteX0010 <raka_088@smkassalaambandung.sch.id>
2024-06-29 18:16:58 +0700

    Patch All Svc Thermal, Mmc

--- core/MTMODS.sh
+++ core/MTMODS.sh
@@ -6,38 +6,29 @@
 # Zyarexx x Visk44
 #___________________________
 
-g() { local l="$1"; shift; echo "$@" > "$l"; }
-h() { chmod "$@"; }
-j='0'; k='1'; m='0000'; n='000'; o='/sys/kernel/eara_thermal'; p='/sys/devices/virtual/thermal/thermal*'; q='/sys/power/cpufreq_'
-r='/proc/mtk-perf/mt_throttle_ms'; s='/proc/perfmgr/boost_ctrl/eas_ctrl'; t='/sys/module'; u='/sys/kernel/tracing'; v='/proc/driver/thermal*'; w='/proc/irq/305/mtk-thermal*'; x='/proc/ppm/policy/thermal_'
-y='/sys/module/msm_thermal/core_control'; z='/proc/cpufreq/cpufreq_imax_thermal_protect'; A='/sys/class/thermal'; B='/sys/class/thermal/tz-by-name'; C='/policy'; D='step_wise'
+Z1='chmod'
+Z2='find'
+Z3='grep'
+Z4='ps -eo args'
+Z5='resetprop'
+Z6='setprop'
+Z7='stop'
+Z8='write'
+Z9='/sys/devices/virtual/thermal/thermal_zone*/'
+ZA='mode'
+ZB='type'
+ZC='echo'
+ZD='date'
+ZE='/storage/emulated/0/MvastThermods.log'
 
-rexx () {
-h 0755 $o $o/enable
-h $m $p
-h $n $q{min,max}_limit
-h $m $p/* $p/*/* $v $v/* $v/*/* $w $w/* $w/*/* $x{limit,cur_power}
-g $o/enable $j; g $o/fake_throttle $k; g $r $j
-for i in debug_schedplus_{up,down}_throttle perfserv_schedplus_{up,down}_throttle; do g $s/$i $j; done
-g $t/dm_snapshot/parameters/snapshot_copy_throttle $j
-g $u/events/power/powernv_throttle/enable $j
-g $u/instances/{wifi,bootreceiver}/events/power/powernv_throttle/enable $j
-g $y/{cpus_offlined,enabled} $j; g $z $j
+touch /storage/emulated/0/MvastThermods.log
 
-therm_pol_set() {
-[[ -d "$therm" ]] && for i in xo skin camera-{f,flash} rf-pa{0,1} modem{0,1}-pa{0,1,2} modem1-{qfe-wtr,modem,mmw{0,1,2,3},skin,pa-{mdm,wtr}} aoss0 cpu-{0,1}-{0,1,2,3} gpu cpuss-{0,1,2} lmh-dcvs-{00,01} quiet sdm charger-skin conn; do
-g "$B/$i-therm-adc$C" $D
-done
-}
+f1() { $Z1 644 $($Z2 $Z9 -name $ZB); }
+f2() { for s in $($Z4 | $Z3 thermal | sed '/grep thermal/d'); do $Z7 $s; $Z5 ctl.$Z7 $s; $Z6 ctl.$Z7 $s; done; }
+f3() { for m in $Z9$ZA; do $Z8 $m "disabled"; done; }
+f4() { $ZC "0" > /sys/class/kgsl/kgsl-3d0/throttling; $ZC "performance" > /sys/class/devfreq/mmc0/governor; $ZC "performance" > /sys/class/devfreq/mmc1/governor; }
+f5() { $ZC "N" > /sys/module/msm_thermal/parameters/enabled; $ZC "0" > /sys/module/msm_thermal/core_control/enabled; $ZC "0" > /sys/module/msm_thermal/vdd_restriction/enabled; }
+f6() { for t in $($Z2 $Z9 -name $ZB); do $Z1 -R 000 $t; done; }
+f7() { $ZC "Thermal patched, executed on $($ZD)" >> $ZE; }
 
-[[ "$1" == "$k" ]] && h 644 "$A/thermal_message/board_sensor_temp"
-pm enable com.xiaomi.gamecenter.sdk.service/.PidService >/dev/null 2>&1 &
-g "$A/thermal_message/board_sensor_temp" "36000"
-h $n "$A/thermal_message/board_sensor_temp"
-pm clear com.xiaomi.gamecenter.sdk.service
-pm disable com.xiaomi.gamecenter.sdk.service/.PidService >/dev/null 2>&1 &
-
-sleep 30
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Mvast Thermods' 'Tag' 'Mvast Thermods Installed Successfully'"
-}
-rexx > /dev/null 2>&1
+f1; f2; f3; f4; f5; f6; f7;
